21eabefbc000aee1b7f90cc104778cf3527fc37e
Julien Miotte <mike.perdide@gmail.com>
2012-10-06 19:36:50 +0200

    Adding a .c test file for function definition style checks.

---
+++ tests/functiondefstyle.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    int my_var = 2;
+
+    printf("Hello world\n");
+    return 0;
+}
+int pain() {
+    int my_var = 3;
+    printf("Hello world\n");
+    return 0;
+}
+int vain(){
+    int my_var = 5;
+
+
+    printf("Hello world\n");
+    return 0;
+}
+int drain()
+{
+    printf("Hello world\n");
+    return 0;}
+int cain()
+ {
+    printf("Hello world\n");
+    return 0;
+ }
+int nain()
+
+ {
+    printf("Hello world\n");
+    return 0;
+ }
+/*
+ * TESTS RESULTS:
+ * 10:12: E723 Check function def style ('{' on function declaration line)
+ * 12:1: E723 Missing blank line after local variable declarations
+ * 15:11: E723 Check function def style ('{' on function declaration line)
+ * 25:14: E723 Check function def style ('}' not in col 1)
+ * 27:2: E723 Check function def style ('{' not in col 1)
+ * 30:2: E723 Check function def style ('}' not in col 1)
+ * 32:1: E723 Check function def style (blank line before '{')
+ * 33:2: E723 Check function def style ('{' not in col 1)
+ * 36:2: E723 Check function def style ('}' not in col 1)
+ */
+
