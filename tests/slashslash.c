daa605cfafd48673a91299b4dc158f3d85c83925
Julien Miotte <mike.perdide@gmail.com>
2012-10-06 19:44:38 +0200

    Adding the expected tests results for the slashslash.c testfile.

--- tests/slashslash.c
+++ tests/slashslash.c
@@ -10,3 +10,9 @@ int main() // this is the main function
      *  // this shouldn't be detected
      */
 }
+/*
+ * TESTS RESULTS:
+ * 3:12: E601 Never use C++ style // one-line comments
+ * 5:5: E601 Never use C++ style // one-line comments
+ * 6:30: E601 Never use C++ style // one-line comments
+ */
